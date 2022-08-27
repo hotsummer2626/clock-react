26a4015d9887d43ab45a3d495bf0bcb5ecb1e328
SUNNY <hotsummer2626@gmail.com>
2022-08-27 22:03:11 +1000

    finish the wall clock part for clocks project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import styled from "styled-components";
+import ClockOne from "./components/ClockOne/ClockOne";
+
+const Container = styled.div`
+  display: grid;
+  /* grid-template-columns: repeat(2, 1fr); */
+`;
 
 const App = () => {
-  return <div>asdfasfdsdf</div>;
+  const [date, setDate] = useState(new Date());
+
+  useEffect(() => {
+    setInterval(() => {
+      setDate(new Date());
+    }, 1000);
+  }, []);
+
+  const dateObj = {
+    second: date.getSeconds(),
+    minute: date.getMinutes(),
+    hour: date.getHours(),
+  };
+
+  return (
+    <Container>
+      <ClockOne {...dateObj} />
+    </Container>
+  );
 };
 
 export default App;
---
+++ src/components/ClockOne/ClockOne.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import styles from "./ClockOne.module.scss";
+
+const numbersData = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+const ClockOne = ({ second, minute, hour }) => {
+  let secondDeg = `${(360 / 60) * second}deg`;
+  let minuteDeg = `${(360 / 60) * minute + (0.5 / 60) * second}deg`;
+  let hourDeg = `${(360 / 12) * hour + minute / 2}deg`;
+  second = second.toString().padStart(2, "0");
+  minute = minute.toString().padStart(2, "0");
+  hour = hour.toString().padStart(2, "0");
+  return (
+    <div className={styles.background}>
+      <div className={styles.container}>
+        <ul className={styles.wallClockWrapper}>
+          {numbersData.map((number, index) => (
+            <li
+              key={index}
+              style={{ "--i": number }}
+              className={styles.numberWrapper}
+            >
+              <span className={styles.number}>{number}</span>
+            </li>
+          ))}
+          <div style={{ "--sec": secondDeg }} className={styles.second}></div>
+          <div style={{ "--min": minuteDeg }} className={styles.minute}></div>
+          <div style={{ "--hr": hourDeg }} className={styles.hour}></div>
+          <div className={styles.centerDot}></div>
+        </ul>
+        <div className={styles.eClockWrapper}>
+          <span className={styles.hour}>{hour}</span>
+          <span className={styles.minute}>{minute}</span>
+          <span className={styles.second}>{second}</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ClockOne;
