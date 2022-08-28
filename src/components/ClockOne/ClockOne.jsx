78f4a02308c701d2f2671b7c5c0fefff2b58b0e1
SUNNY <hotsummer2626@gmail.com>
2022-08-28 22:36:15 +1000

    finish the clockOne

--- src/components/ClockOne/ClockOne.jsx
+++ src/components/ClockOne/ClockOne.jsx
@@ -7,6 +7,9 @@ const ClockOne = ({ second, minute, hour }) => {
   let secondDeg = `${(360 / 60) * second}deg`;
   let minuteDeg = `${(360 / 60) * minute + (0.5 / 60) * second}deg`;
   let hourDeg = `${(360 / 12) * hour + minute / 2}deg`;
+  let ampm = hour <= 12 ? "am" : "pm";
+  hour = hour % 12;
+  hour = hour ? hour : 12;
   second = second.toString().padStart(2, "0");
   minute = minute.toString().padStart(2, "0");
   hour = hour.toString().padStart(2, "0");
@@ -30,8 +33,11 @@ const ClockOne = ({ second, minute, hour }) => {
         </ul>
         <div className={styles.eClockWrapper}>
           <span className={styles.hour}>{hour}</span>
+          <span>:</span>
           <span className={styles.minute}>{minute}</span>
+          <span>:</span>
           <span className={styles.second}>{second}</span>
+          <span className={styles.ampm}>{ampm}</span>
         </div>
       </div>
     </div>
